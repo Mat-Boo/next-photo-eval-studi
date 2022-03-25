497c4ffd47cec13e63c68de321e50ee152d48123
Mathieu Bouthors <mathieubouthors@hotmail.com>
2022-03-25 10:28:31 +0100

    Add _error page

---
+++ pages/_error.js
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from '../styles/404.module.scss';
+import Image from 'next/image';
+import Head from 'next/head';
+
+export default function _error() {
+    return (
+        <>
+            <Head>
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <meta content="NOINDEX, NOFOLLOW" name="robots" />
+                <title>PAGE INTROUVABLE | Charles Cantin - Photographe</title>
+                <meta name="description" content="Photographe professionnel, spécialisé en portrait. Shooting en studio, reportage naturel en extérieur ou à domicile, mariage, photo d'entreprise." />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <div className={styles.page404}>
+                <div className={styles.logo404}>
+                    <span>4</span>
+                    <div className={styles.lense}>
+                        <Image src='/resources/404camera.png' alt='cameraLense' width='519' height='519'/>
+                    </div>
+                    <span>4</span>
+                </div>
+                <h1>PAGE INTROUVAB</h1>
+                <span>Cette page n'existe pas !</span>
+                <Link href='/'>
+                    <a>Retourner à l'accueil</a>
+                </Link>
+            </div>
+        </>
+    )
+}
