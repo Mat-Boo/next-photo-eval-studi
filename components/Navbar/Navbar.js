904cd154a6b47ef1c381242e37b7fd1ba1edf8cd
Mathieu Bouthors <mathieubouthors@hotmail.com>
2022-03-28 11:35:56 +0200

    Fix hide miniMenu on click on logo & verify if click on closeMiniMenu when be on contact page with policy open to leave scroll desactive on body

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -31,27 +31,22 @@ export default function Navbar() {
                 dispatch(updatePresta(''))
             }
         }
-        activeOverflow();
-    }
-
-    const activeOverflow = () => {
-        if (document.body.style.overflow === 'hidden') {
+        if (document.body.style.overflow === 'hidden' && e.target.id !== 'closeMiniMenu') {
             document.body.style.overflow = 'auto';
         }
-
     }
 
     return (
         <nav className={styles.navbar}>
             <Link href='/'>
-                <a  className={styles.logoNav} onClick={activeOverflow}>
+                <a  className={styles.logoNav} onClick={(e) => hideMiniMenu(e)}>
                     <Image src='/resources/LogoNavbar.png' alt='logo' width='833' height='452'/>
                 </a>
             </Link>
             {!miniMenu ? <svg onClick={(e) => displayMiniMenu(e)} xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className={styles.miniMenuBtn} viewBox="0 0 16 16">
                 <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
             </svg> :
-            <svg onClick={(e) => hideMiniMenu(e)} xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className={styles.closeMiniMenu} viewBox="0 0 16 16">
+            <svg onClick={(e) => hideMiniMenu(e)} id='closeMiniMenu' xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" className={styles.closeMiniMenu} viewBox="0 0 16 16">
                 <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
             </svg>
             }
