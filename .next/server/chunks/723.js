64151c56fa2549d1062aca2ab4e3a7a69565eead
Mathieu Bouthors <mathieubouthors@hotmail.com>
2025-04-08 10:05:34 +0200

    Add Auxseen in footer

---
+++ .next/server/chunks/723.js
@@ -0,0 +1,21 @@
+exports.id = 723;
+exports.ids = [723];
+exports.modules = {
+
+/***/ 723:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"page404": "__404_page404__mRjD2",
+	"fadein": "__404_fadein__5hGmL",
+	"logo404": "__404_logo404__5DMua",
+	"lense": "__404_lense__Sz_e6",
+	"slidein": "__404_slidein__uUA_C"
+};
+
+
+/***/ })
+
+};
+;
--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -4,7 +4,15 @@ import styles from './Footer.module.scss';
 export default function Footer() {
     return (
         <footer className={styles.footer}>
-            <span className={styles.copyright}>© 2022 Charles Cantin | Créé par Mathieu Bouthors</span>
+            <span className={styles.copyright}>© 2022 Charles Cantin | Créé par Mathieu Bouthors | </span>
+            <a
+                href='https://www.auxseen.com'
+                className={styles.auxseenLink}
+                target="_blank"
+                rel="noopener noreferrer"
+            > 
+                Auxseen.
+            </a>
         </footer>
     )
 }
